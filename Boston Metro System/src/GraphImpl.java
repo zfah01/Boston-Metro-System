ce44a1177b394a430f4b8fcc2a837f6ba9abcbff
Saeed Alkaabi <saeed.alkaabi.2019@uni.strath.ac.uk>
2022-03-11 16:57:27 +0000

    *

---
+++ Boston Metro System/src/GraphImpl.java
@@ -0,0 +1,31 @@
+import java.util.List;
+public class GraphImpl implements Graph {
+
+    public class GraphImpl {
+        public List<Node> getAdjacentNodes(Node) {}
+
+        public List<Node> getAllNodes() {}
+
+        public boolean isGraphEmpty() {}
+
+        public void addNode(Node node) {}
+
+        public void removeNode(Node node) {}
+
+        public void addEdge(LineID, Node, Node, Weight) {}
+
+        public void removeEdge(LineID, Node, Node, Weight) {}
+
+        public boolean isEdge(LineID, Node, Node) {}
+
+        public boolean isNode(Node node) {}
+
+        public int getEdgeWeight(Node, Node) {}
+
+        public int getNodeCount() {}
+
+        public int getEdgeCount() {}
+
+
+    }
+}
