47f23e269077cd000e9e8e6fe2853784ce6756b7
Abdulrahman Hamed M Alsaidalani CS2019 <abdulrahman.alsaidalani.2019@uni.strath.ac.uk>
2022-03-23 16:09:21 +0000

    Update Node.java

--- Boston Metro System/src/Node.java
+++ Boston Metro System/src/Node.java
@@ -1,8 +1,71 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-interface Node{
-    void setNodeName(String nodeName);
-    String getNodeName();
+/**
+ * The Node class represents a station
+ * in this tutorial and will as such have
+ * a string representing the station's name.
+ * As well as an ArrayList of nodes that will store
+ * any instantiated nodes children.
+ */
+public class Node {
+	
+	private int id;
+    public String stationName;
+    private String color;
+    private int vertex;
+	private int cost;
+    
 
-    void setNodeID(int id);
-    int getNodeID();
+    public Node(int id, String stationName, String color, int vertex, int cost) {
+		super();
+		this.id = id;
+		this.stationName = stationName;
+		this.color = color;
+		this.vertex = vertex;
+		this.cost = cost;
+	}
+
+    //    An auxiliary function which allows
+    //    us to remove any child nodes from
+    //    our list of child nodes.
+    public boolean removeChild(Node n){
+        return false;
+    }
+
+	public int getCost() {
+		return cost;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStationName() {
+		return stationName;
+	}
+
+	public void setStationName(String stationName) {
+		this.stationName = stationName;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getVertex() {
+		return vertex;
+	}
+
+	public void setVertex(int vertex) {
+		this.vertex = vertex;
+	}
 }
